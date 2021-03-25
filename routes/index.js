e3652d14238d99ac8d55ee13d2828048433c7483
Terrence Childs <tchilds1108@gmail.com>
2021-03-25 11:06:50 -0400

    finished

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express'),
 router = express.Router();
 
 router.get('/', (req, res) => {
-res.json('Welcome to my API').status(200);
+    res.json('Welcome to my API').status(200);
 });
 
 module.exports = router;
